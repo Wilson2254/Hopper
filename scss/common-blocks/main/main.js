5b1861199ca8cb4c7216d0c0829200fd5bbe5268
Alexander_P <sanek191296@mail.ru>
2020-11-02 13:23:33 +0300

    main

--- scss/common-blocks/main/main.js
+++ scss/common-blocks/main/main.js
@@ -1,7 +1,7 @@
 //Точка входа всех модулей
 import { inputValidStorage } from './input-area/input/input.js'; //Работа с полями
-import { sendForm } from './work-detail/buttons/__send/buttons__send.js' //Скрыть Алерт и показать Done
 import { alert } from './alert/alert.js' //Работа с алертом
+import { sendForm } from './work-detail/buttons/__send/buttons__send.js' //Скрыть Алерт и показать Done
 inputValidStorage();
 alert();
 sendForm();
